619474612975cb6b18a6a72229748185bdce735d
Billy <st424204@yahoo.com.tw>
2019-10-02 10:52:22 +0000

    Reduce memory use of qemu

--- knote/docker/share/run.sh
+++ knote/docker/share/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 exec timeout 300 qemu-system-x86_64 \
-    -m 256M \
+    -m 128M \
     -nographic \
     -kernel /home/knote/bzImage \
     -append 'console=ttyS0 loglevel=3 oops=panic panic=1 kaslr' \
     -monitor /dev/null \
     -initrd /home/knote/initramfs.cpio.gz  \
-    -smp cores=4,threads=4 \
+    -smp cores=2,threads=2 \
     -cpu kvm64,smep,smap
